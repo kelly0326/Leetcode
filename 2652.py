6016d5791257aea718a6b9ba744e87ab0a73c699
Kelly liu <shu@liu.net>
2025-05-20 09:02:19 -0700

    Python for loop, if() & def()

---
+++ 2652.py
@@ -0,0 +1,13 @@
+class Solution:
+    def sumOfMultiples(self, n: int) -> int:
+        lst = []
+        for i in range(1, n+1):
+            if i % 3 == 0 or i % 5 == 0 or i % 7 == 0:
+                # print(i)
+                lst.append(i)
+        print(lst) # 3 5 6 7
+        sum = 0
+        for i in range(len(lst)):
+            sum = sum + lst[i]
+        print(sum)
+        return sum
