144c6d9f1cc6a1ea96eb4d03452e067b3ae8abe1
Kelly liu <shu@liu.net>
2025-06-04 10:21:41 -0700

    Python practice continued

--- 1672.py
+++ 1672.py
@@ -1,3 +1,4 @@
+from typing import List
 class Solution:
     def maximumWealth(self, accounts: List[List[int]]) -> int:
         # print(accounts) 
@@ -13,3 +14,8 @@ class Solution:
                 largest = lst[i]
         print(largest)
         return largest
+accounts = [[1,2,3],
+            [3,2,4]
+                    ]
+mydoor = Solution()
+print(mydoor.maximumWealth(accounts))
